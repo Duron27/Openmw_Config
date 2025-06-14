3029a60d043b50f1813a21378ef893f2fde9a4b1
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-06-14 06:54:24 -0700

    CLEANUP: Use a constant for no config dir

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 mod config;
 pub use config::OpenMWConfiguration;
 
-pub (crate) trait GameSetting: std::fmt::Display {
+pub(crate) trait GameSetting: std::fmt::Display {
     fn meta(&self) -> &GameSettingMeta;
 }
 
 #[derive(Debug, Clone)]
 pub struct GameSettingMeta {
     source_config: std::path::PathBuf,
+    
 }
+
+const NO_CONFIG_DIR: &str = "FAILURE: COULD NOT READ CONFIG DIRECTORY";
 
 /// Path to input bindings and core configuration
 /// These functions are not expected to fail and should they fail, indicate either:
@@ -16,13 +19,9 @@ pub struct GameSettingMeta {
 /// or that an unsupported system is being used.
 pub fn default_config_path() -> std::path::PathBuf {
     if cfg!(windows) {
-        dirs::document_dir()
-            .expect("FAILURE: COULD NOT READ CONFIG DIRECTORY")
-            .join("openmw")
+        dirs::document_dir().expect(NO_CONFIG_DIR).join("openmw")
     } else {
-        dirs::preference_dir()
-            .expect("FAILURE: COULD NOT READ CONFIG DIRECTORY")
-            .join("openmw")
+        dirs::preference_dir().expect(NO_CONFIG_DIR).join("openmw")
     }
 }
 
