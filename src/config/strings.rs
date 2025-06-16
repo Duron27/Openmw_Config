b80ce6ec213ff8e3f88095d7ac45626114f53de4
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-06-16 16:09:48 -0700

    CLEANUP: Remove Dead code

--- src/config/strings.rs
+++ src/config/strings.rs
@@ -1,54 +1,6 @@
-use std::{fmt::Write, path::PathBuf};
-
-// Private helper for error handling when writing to String
-fn write_line_io(f: &mut String, line: String) -> Result<(), String> {
-    writeln!(f, "{line}").map_err(|_| format!("Failed writing config line: {line:?}"))
-}
-
-pub fn resources(f: &mut String, resources: &Option<PathBuf>) -> Result<(), String> {
-    if let Some(resources) = resources {
-        return write_line_io(f, format!("resources={}", resources.display()));
-    } else {
-        Ok(())
-    }
-}
-
-pub fn userdata(f: &mut String, userdata: &String) -> Result<(), String> {
-    write_line_io(f, format!("userdata={}", userdata))
-}
-
-pub fn data_local(f: &mut String, data_local: &Option<PathBuf>) -> Result<(), String> {
-    if let Some(data_local) = data_local {
-        write_line_io(f, format!("data-local={}", data_local.display()))
-    } else {
-        Ok(())
-    }
-}
-
-pub fn fallback_archive(f: &mut String, fallback_archive: &String) -> Result<(), String> {
-    write_line_io(f, format!("fallback-archive={}", fallback_archive))
-}
-
-pub fn data_directory(f: &mut String, data_directory: &PathBuf) -> Result<(), String> {
-    write_line_io(f, format!("data={}", data_directory.display()))
-}
-
-pub fn content_file(f: &mut String, content_file: &String) -> Result<(), String> {
-    write_line_io(f, format!("content={}", content_file))
-}
-
-pub fn fallback_entry(f: &mut String, key: &String, value: &String) -> Result<(), String> {
-    write_line_io(f, format!("fallback={},{}", key, value))
-}
-
-pub fn write_comments(comments: Option<Vec<String>>, config_string: &mut String) {
-    if let Some(comments) = comments {
-        for comment in comments {
-            config_string.push_str(&comment);
-            config_string.push('\n');
-        }
-    }
-}
+/// This whole file should just be part of DirectorySetting now and the parse_data_directory function exposed in the lib.rs
+/// But right now, meh
+use std::path::PathBuf;
 
 fn strip_special_components<P: AsRef<std::path::Path>>(input: P) -> PathBuf {
     let mut result = PathBuf::new();
